601b32e1adf00c494dab0e887728d78750b4281c
tianws <tianws@mapbar.com>
2017-07-27 16:28:32 +0800

    加入send_ip.py脚本

---
+++ README.md
@@ -0,0 +1,5 @@
+# 脚本备份
+## send_ip.py
+在/etc/rc.local加入
+python send_ip.py
+可以系统启动自动发IP给指定邮箱
---
+++ send_ip.py
@@ -0,0 +1,63 @@
+#/bin/env python
+# -*-coding:utf8-*-
+import socket
+import fcntl
+import time
+import struct
+import smtplib
+import urllib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+import commands
+
+#发送邮件的基本函数，参数依次如下
+# smtp服务器地址、邮箱用户名，邮箱，发件人地址，手贱儿女地址（列表的方式），邮件主题，邮件html内容
+def sendEmail(smtpserver,username,password,sender,receiver,subject,msghtml):
+    msgRoot = MIMEMultipart('related')
+    msgRoot["To"] = ','.join(receiver)
+    msgRoot["From"] = sender
+    msgRoot['Subject'] =  subject
+    msgText = MIMEText(msghtml,'html','utf-8')
+    msgRoot.attach(msgText)
+    #sendEmail
+    smtp = smtplib.SMTP()
+    smtp.connect(smtpserver)
+    smtp.login(username, password)
+    smtp.sendmail(sender, receiver, msgRoot.as_string())
+    smtp.quit()
+
+# 检查网络连同性
+def check_network():
+    while True:
+        try:
+            result=urllib.urlopen('http://baidu.com').read()
+            print result
+            print "Network is Ready!"
+            break
+        except Exception , e:
+           print e
+           print "Network is not ready,Sleep 5s...."
+           time.sleep(5)
+    return True
+
+# 获得本级制定接口的ip地址
+def get_ip_address():
+    #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #s.connect(("1.1.1.1",80))
+    #print s.getsockname()
+    #ipaddr=" ".join(str(d) for d in s.getsockname())
+    #s.close()
+    hostname = commands.getstatusoutput('hostname')
+    hostname = str(hostname[1])
+    ipaddr = commands.getstatusoutput('hostname -I')
+    #ipaddr=" ".join(str(d) for d in ipaddr)
+    ipaddr=str(ipaddr[1])
+    content = hostname+" : "+ipaddr
+    print content
+    return content
+
+if __name__ == '__main__':
+    check_network()
+    content=get_ip_address()
+    sendEmail('smtp.163.com','pi_raspberrypi@163.com','111qqq','pi_raspberrypi@163.com',['tianws@mapbar.com'],'IP Address Of Raspberry Pi',content)
