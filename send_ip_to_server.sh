248c209e165d7d6236755fc2a724c9b28afa72d1
tianws <tianws@mapbar.com>
2017-07-31 12:40:53 +0800

    send_ip_to_server增加判断网络联通的逻辑

--- send_ip_to_server.sh
+++ send_ip_to_server.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+timeout=5
+target=www.baidu.com
+ret_code=0
+
+while [[ "$ret_code"x != "200"x ]]
+do
+    ret_code=`curl -I -s --connect-timeout $timeout $target -w %{http_code} | tail -n1`
+done
+
 TEXTNAME=$(hostname)"_IP.txt" &&
 date > $TEXTNAME &&
 hostname -I >> $TEXTNAME &&
